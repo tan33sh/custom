35f1b5ebd41b2b0a5cad4d0f819a8dee95ce3aad
tan33sh <tan33sh@3547758.no-reply.drupal.org>
2018-01-07 02:52:12 +0530

    Fixed all coding standard violations.

--- src/Controller/CustomController.php
+++ src/Controller/CustomController.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
 
 namespace Drupal\custom\Controller;
 
+/**
+ * CustomController Class.
+ */
 class CustomController {
-	public function text() {
-		return array(
-			'#title' => "This is a custom module",
-			'#markup' => 'Functional testing of this module.'
-		);
 
-	}
+  /**
+   * To display text.
+   */
+  public function text() {
+
+    return array(
+    '#title' => "This is a custom module",
+    '#markup' => 'Functional testing of this module.'
+    );
+
+  }
+
 }
