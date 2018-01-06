a197629e8a6c95ff0ca72130b8cf5533bfc9a90f
tan33sh <tan33sh@3547758.no-reply.drupal.org>
2018-01-07 02:51:34 +0530

    Fixed all coding standard violations

--- src/Tests/CustomTest.php
+++ src/Tests/CustomTest.php
@@ -1,43 +1,37 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\custom\Tests\CustomTest.
- */
-
 namespace Drupal\custom\Tests;
 
 use Drupal\simpletest\WebTestBase;
 
 /**
  * Provide some basic tests for our custom module.
+ *
  * @group custom
  */
-
 class CustomTest extends WebTestBase {
 
-	/**
+  /**
    * Modules to install.
+   *
    * @var array
    */
+  public static $modules = ['custom', 'node'];
 
-	public static $modules = ['custom', 'node'];
-
-	/**
+  /**
    * Tests that 'custom' returns a 200.
    */
-	
-	public function testCustomRouterURLIsAccessible() {
-		$this->drupalGet('custom');
-		$this->assertResponse(200, 'User is able to access the URL');
-	}
+  public function testCustomRouterUrlIsAccessible() {
+    $this->drupalGet('custom');
+    $this->assertResponse(200, 'User is able to access the URL');
+  }
 
-	/**
+  /**
    * Tests that the form has some text.
    */
+  public function testCustomText() {
+    $this->drupalGet('custom');
+    $this->assertText('Functional testing of this module.', 'Message is shown');
+  }
 
-	public function testCustomText(){
-		$this->drupalGet('custom');
-		$this->assertText('Functional testing of this module.', 'Message is shown');
-	}
 }
